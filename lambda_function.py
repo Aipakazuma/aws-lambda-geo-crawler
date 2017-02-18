faf6ab4e1814d3aa5d5acb4ff5b1e6a7b152f168
玉城一磨 <kazuma.tamashiro@azana.co.jp>
2017-02-18 14:13:05 +0900

    [add] s3で色々遊ぶ２

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 # lambda_function.py
 import boto3
 
@@ -9,18 +10,24 @@ def lambda_handler(event, context):
   print bucket.name
 
   # list
+  # 階層を作ると、一番最下層までファイルを読み出してくれる
   for object in bucket.objects.all():
     print object.key
 
   # upload
-  bucket.upload_file('./event.json', 'event.json')
+  # bucket.upload_file('./event.json', 'event.json')
+  # ディレクトリを追記したら、一緒に追加された
+  bucket.upload_file('./event.json', 'test/event.json')
+  # 存在するディレクトリを追記したままでも上書きされず、ファイルだけが追加される
+  bucket.upload_file('./event.json', 'test/event2.json')
+  bucket.upload_file('./event.json', 'test/test2/event.json')
 
   # download
-  bucket.download_file('event.json', 'event2.json')
+  # bucket.download_file('event.json', 'event2.json')
 
   # delete
+  # 階層が存在しても削除することができる
   for object in bucket.objects.all():
     object.delete()
-
 
   return
